a1c9094e523aba78b4c671ad17e16f45db558abd
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2024-01-27 13:13:36 -0300

    Dados de auditoria

--- src/main/java/com/devsuperior/DSCatalog/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/DSCatalog/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.devsuperior.DSCatalog.controllers.exceptions;
 
+import com.devsuperior.DSCatalog.services.exception.DatabaseException;
 import com.devsuperior.DSCatalog.services.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -15,11 +16,23 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomErrorDTO> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
         CustomErrorDTO err = new CustomErrorDTO();
+        HttpStatus status = HttpStatus.NOT_FOUND;
         err.setTimestamp(Instant.now());
-        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setStatus(status.value());
         err.setError(e.getMessage());
         err.setPath(request.getRequestURI());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomErrorDTO> Database(DatabaseException e, HttpServletRequest request){
+        CustomErrorDTO err = new CustomErrorDTO();
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
     }
 
 }
